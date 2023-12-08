a34efbb5729fff5a3947718e6050b8eee2d295d3
Eric <email@liangjielai@gmail.com>
2023-12-09 03:56:47 +0800

    Fourth commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload3rdupdate.
+          Edit <code>src/App.js</code> and save to reload4rdupdate.
         </p>
         <a
           className="App-link"
